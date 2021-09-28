89c0084567d613fee215193a39655bfa251d8981
chippa2801 <66907368+chippa2801@users.noreply.github.com>
2021-09-28 08:33:18 +0530

    Revert "Update ProgrammingAssignment2.R"
    This reverts commit b0856259ff807e244d236c7b60fe7666d55f3c4d.

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,36 +1,35 @@
-##The first function, makeCacheMatrix creates a special "Matrix" object, that can cache it's inverse.
+##The first function, makeCacheMatrix creates a special Matrix object, that can cache it's inverse.
 # it contains
-#set,get,getinve,setinve
+#set,get,getinv,setinv
 #library(mass) it allows us to calculate inverse both squared and non-squared matrix.
 install.packages("MASS")
 library(MASS)
-makeCacheMatrix <- function(p = matrix()){
-  inve = NULL     # keeping inverse as null at first.
-  set = function(q){
-                    p<<-q
-                    inve<<-NULL
+makeCacheMatrix <- function(x = matrix()){
+  inv = NULL
+  set = function(y){
+                    x<<-y
+                    inv<<-NULL
                    }
-  get    <- function()p      # this line is to get the matrix.
-  setinve <- function(inverse)inve<<-inverse
-  getinve <- function(){
-                      inver <- ginve(p)
-                      inver%*%p    # this is to get the matrix inverse
+  get    <- function()x
+  setinv <- function(inverse)inv<<-inverse
+  getinv <- function(){
+                      inver <- ginv(x)
+                      inver%*%x
                       }
   list(set = set,get = get,
-       setinve = setinve,
-       getinve = getinve)
+       setinv = setinv,
+       getinv = getinv)
 }
-# write some comments describing the function
+
 #This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-#cachesolve returns the  cache data.
-cacheSolve <- function(p, ...){   ## gets the cache data
-  inve <- p$getinve()
-  if(!is.null(inve)){
+cacheSplve <- function(x, ...){
+  inv <- x$getinv()
+  if(!is.null(inv)){
                     print("getting the cache data!")
-                    return(inve)    ## it returns the values i.e inverse
+                    return(inv)
                     }
- data <- p$get()
- inve <- solve(data,...)      ## it calculates the inverse values.
- p$setinve(inve)
- inve     # it returns what we are looking for that is inverse of the matrix(p)
+ data <- x$get()
+ inv <- solve(data,...)
+ x$setinv(inv)
+ inv
 }
